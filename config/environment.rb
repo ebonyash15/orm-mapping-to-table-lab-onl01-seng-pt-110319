de8f5e5ea3c798dbd893faf0085bec15e1bdb73b
Ebony Sharrock <ebonysharrock@gmail.com>
2020-02-01 02:19:05 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 require 'bundler'
 Bundler.require
+require 'sqlite3'
+require_relative '../lib/song.rb'
 
-require_relative '../lib/student'
+require_relative '../lib/student.rb'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
