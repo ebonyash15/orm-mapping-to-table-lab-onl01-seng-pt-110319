12c97e3218a0799729e5dd9fec906615c610de6d
Ebony Sharrock <ebonysharrock@gmail.com>
2020-02-01 12:52:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,41 +1,44 @@
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
   attr_accessor :name, :grade
   attr_reader :id
+  
   def initialize(name, grade, id=nil)
     @id=id
     @name=name
     @grade=grade
   end
+  
   def self.create_table
-    #Use heredoc
-    # sql = #SQL statement for mapping table
-    # attributes of a student, name, grade, and id,
-    #should correspond to the column names you are creating in
-    #your students table.
-    #The id column should be the primary key.\
-
+    sql = <<-SQL 
+      CREATE TABLE students
+        (id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT)
+    SQL
     self.execute
   end
+  
   def self.execute
     #sql natural execute statement DB[:conn]
     DB[:conn].execute(sql)
   end
+  
   def self.drop_table
-    #sql=SQLstmt to remove table
+    DROP TABLE students
     self.execute
   end
+  
   def save
-    # saves the attributes describing a given student to the students table in our database
-    # sql=SQL statement that will INSERT the correct data into the table
-    #bound paremeters to pass the given student's name and grade into the SQL statement
-    #grab the ID of the last inserted row  assign it to the be the value of the @id attribute of the instance
+    sql = <<-SQL
+    INSERT INTO students (name, grade)
+    Values (@name, @grade)
+    SELECT id FROM students ORDER BY DESC LIMIT 1
+    SQL
+    sql = @id
   end
+  
   def self.create( :name, :grade)
-    # instantiate a new Student object with Student.new(name, grade) and 2) save
-    #that new student object via student.save. The #create method should return
-    #the student object that it creates
+    Student.new(name, grade)
+    Student.save
   end
 end
