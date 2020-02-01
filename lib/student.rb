31f8a186dcbd3fbb9ea300abec9fbcd48eec0d56
Ebony Sharrock <ebonysharrock@gmail.com>
2020-02-01 13:12:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,17 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   @@all=[]
-  
+
   def initialize(name, grade, id=nil)
     @id=id
     @name=name
     @grade=grade
   end
-  
+
   def self.save
     @@all << self
   end
-  
+
   def self.create_table
     sql = <<-SQL
       CREATE TABLE students
